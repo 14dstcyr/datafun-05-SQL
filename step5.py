d27dc1594d859ba8806a989d3eef8ac7542686f4
Deb St. Cyr <S568800@nwmissouri.edu>
2024-02-05 20:38:35 -0600

    update commit

--- step5.py
+++ step5.py
@@ -5,7 +5,10 @@ py -m venv .venv
 .\.venv\Scripts\Activate.ps1
 
 # Install Panda and Arrow
-py -m pip install pandas pyarrow
+py -m pip install pandas 
+py -m pip install numpy
+py -m pip install seaborn
+py -m pip install matplotlib 
 
 # Execute freeze
 py -m pip freeze > requirements.txt
