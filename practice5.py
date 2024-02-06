6d29de7f620a5de1205950c328f953d15b5ff5fc
Deb St. Cyr <S568800@nwmissouri.edu>
2024-02-05 21:16:34 -0600

    update commit

---
+++ practice5.py
@@ -0,0 +1,14 @@
+# Initialize venv
+py -m venv .venv
+
+# Activate powershell script
+.\.venv\Scripts\Activate.ps1
+
+# Install Panda and Arrow
+py -m pip install pandas 
+py -m pip install numpy
+py -m pip install seaborn
+py -m pip install matplotlib 
+
+# Execute freeze
+py -m pip freeze > requirements.txt
